5a01e00b85cbae03b652336746abd011c60b5146
stothp <stothp@gmail.com>
2020-11-29 20:16:28 +0100

    Error Codes added

---
+++ public_html/api/get_favourite_color/ErrorCodes.php
@@ -0,0 +1,9 @@
+<?php
+
+class ErrorCodes
+{
+    public const INTERNAL_ERROR = 0;
+    public const INVALID_PARAMETERS = 1;
+    public const INVALID_EMAIL = 2;
+    public const INVALID_PASSWORD = 3;
+}
