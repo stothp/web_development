03df49f24c0919e41f1982fc8f21221a70668986
stothp <stothp@gmail.com>
2020-11-29 21:50:05 +0100

    Working index.php added to the endoint

---
+++ public_html/api/get_favourite_color/index.php
@@ -0,0 +1,69 @@
+<?php
+header("Content-Type: application/json");
+
+spl_autoload_register(function ($class_name) {
+    if (file_exists($class_name .'.php')) 
+    {
+        include $class_name . '.php';
+    }
+});
+
+if (!getenv("WEBFEJLESZTES_BASE") || !getenv("WEBFEJLESZTES_CONFIG"))
+{
+    echo json_encode(array("error_code" => ErrorCodes::INTERNAL_ERROR), JSON_FORCE_OBJECT);
+    return;
+} 
+
+if (!$ini = @parse_ini_file(getenv("WEBFEJLESZTES_BASE") . "\\" . getenv("WEBFEJLESZTES_CONFIG"), true))
+{
+    echo json_encode(array("error_code" => ErrorCodes::INTERNAL_ERROR), JSON_FORCE_OBJECT);
+    return;
+}
+
+if (!isset($_POST["email"], $_POST["password"]))
+{
+    echo json_encode(array("error_code" => ErrorCodes::INVALID_PARAMETERS), JSON_FORCE_OBJECT);
+    return;
+} 
+
+$pw_file = getenv("WEBFEJLESZTES_BASE") . "\\" . $ini["password"]["password_file"];
+
+if (!file_exists($pw_file))
+{
+    echo json_encode(array("error_code" => ErrorCodes::INTERNAL_ERROR), JSON_FORCE_OBJECT);
+    return;
+}
+
+$gc = new ColorQuery(
+        $ini["mysql"]["host"],
+        $ini["mysql"]["username"],
+        $ini["mysql"]["password"],
+        $ini["mysql"]["database"],
+        $ini["mysql"]["port"]);
+
+if (!$gc->query($_POST["email"]))
+{
+    echo json_encode(array("error_code" => ErrorCodes::INTERNAL_ERROR), JSON_FORCE_OBJECT);
+    return;
+}
+       
+if ($gc->getColor() == null)
+{
+    echo json_encode(array("error_code" => ErrorCodes::INVALID_EMAIL), JSON_FORCE_OBJECT);
+    return;
+}
+
+$userList = new UserList($pw_file);
+if (!$user = $userList->get("terpeszterez@citromail.hu"))
+{
+    echo json_encode(array("error_code" => ErrorCodes::INTERNAL_ERROR), JSON_FORCE_OBJECT);
+    return;
+}
+
+if (!$user->checkPassword($_POST["password"]))
+{
+    echo json_encode(array("error_code" => ErrorCodes::INVALID_PASSWORD), JSON_FORCE_OBJECT);
+    return;
+}
+
+echo json_encode(array("color" => $gc->getColor()), JSON_FORCE_OBJECT);
